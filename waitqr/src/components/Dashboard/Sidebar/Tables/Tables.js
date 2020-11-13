5ea41bd93522413420f889e0694dc8cfe005970c
esteban9706 <corlo.esteban@gmail.com>
2020-11-13 10:45:04 -0600

    Tables Card Added

--- waitqr/src/components/Dashboard/Sidebar/Tables/Tables.js
+++ waitqr/src/components/Dashboard/Sidebar/Tables/Tables.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 function Tables() {
     return (
         <div className="container-fluid dashboard-componente mt-4 mb-4">
             <div className="row">
                 <div className="col-md-6 text-left" >
-                    <h1>Aqui van tables </h1>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Mesa 1</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Terraza</Card.Subtitle>
+                            <Card.Text>
+                                Entrada: 13:45<br/>
+                                Orden Procesada
+                        </Card.Text>
+                            <Card.Link href="#">Agregar Platillo</Card.Link>
+                            <Card.Link href="#">Cerrar Mesa</Card.Link>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
         </div>
