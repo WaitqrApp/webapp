45d8bcf52bef1306dcb78f59cf8cde16237bb485
esteban9706 <corlo.esteban@gmail.com>
2020-11-15 23:58:25 -0600

    DishMenu component created

---
+++ waitqr/src/components/Dashboard/Sidebar/Menu/DishMenu.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+
+
+function DishMenu() {
+    return (
+        <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="../img/ordenes.png" />
+                <Card.Body>
+                    <Card.Title>Picaña Loca</Card.Title>
+                    <Card.Text>
+                        Servida con Salsa BBQ y Cebolla Caramelizada
+        </Card.Text>
+                    <Button variant="primary">Editar</Button>
+                    <Button variant="primary">Desactivar</Button>
+                </Card.Body>
+            </Card>
+        </>
+    );
+
+
+}
+
+
+export default DishMenu;
+
--- waitqr/src/components/Dashboard/Sidebar/Menu/Menu.js
+++ waitqr/src/components/Dashboard/Sidebar/Menu/Menu.js
@@ -1,28 +1,36 @@
 import React from "react";
-import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { withRouter } from "react-router";
 import MenuSidebar from './MenuSidebar';
-
+import DishMenu from './DishMenu';
 import './menusidebar.css';
 
 const Dash = props => {
-   
+
 
     return (
         <>
-         <Container fluid>
+            <Container fluid>
                 <Row>
-                    <Col xs={2} id="sidebar-wrapper">      
-                      <MenuSidebar />
+                    <Col xs={2} id="sidebar-wrapper">
+                        <MenuSidebar />
                     </Col>
-                    <Col  xs={10} id="page-content-wrapper">
+                    <Col xs={10} id="page-content-wrapper">
                         <h1>El Santo Chancho</h1>
-                    </Col> 
+                        <DishMenu/>
+
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2}>
+                        <DishMenu/>
+                    </Col>
+
                 </Row>
 
             </Container>
         </>
-        );
-  };
-  const Dashboard = withRouter(Dash);
-  export default Dashboard
+    );
+};
+const Dashboard = withRouter(Dash);
+export default Dashboard
