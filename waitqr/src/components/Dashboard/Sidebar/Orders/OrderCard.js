790aac101283721658dc147cb3ef97678b316914
esteban9706 <corlo.esteban@gmail.com>
2020-11-23 20:49:40 -0600

    OrderCard Component created

---
+++ waitqr/src/components/Dashboard/Sidebar/Orders/OrderCard.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import { Container, Row, Col, Form, Button, Modal, Card } from "react-bootstrap";
+import '../Tables/styles/tables.css';
+
+function OrderCard() {
+  return (
+    <Fragment>
+    <Card className="mt-1" style={{ width: '18rem', height: "18rem", justifyContent: 'center' }}>
+                        <Card.Body>
+                            <Card.Title className="text-center font-weight-bold">
+                                Orden 1
+                            </Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                Terraza
+                            </Card.Subtitle>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                12:30 23-06-2020
+                            </Card.Subtitle>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Picaña Haye" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Cerveza" />
+                            </Form.Group>
+                            <Button variant="primary">Aceptar</Button>
+                            <Button variant="light">Rechazar</Button>
+                        </Card.Body>
+                    </Card>
+    </Fragment>
+
+  );
+}
+
+export default OrderCard;
--- waitqr/src/components/Dashboard/Sidebar/Orders/Orders.js
+++ waitqr/src/components/Dashboard/Sidebar/Orders/Orders.js
@@ -1,42 +1,48 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import '../Tables/styles/tables.css';
-import { Container, Row, Col, Form, Button, Modal } from "react-bootstrap";
-
-
+import OrderCard from './OrderCard';
+import { Dropdown, DropdownButton, Col, Row } from 'react-bootstrap';
 function Orders() {
     return (
         <div className="container-fluid dashboard-componente-mesas mt-4 mb-4">
+            <Row>
+                <Col xs lg ="2">
+                    Ordenes
+                </Col>
+                <Col md = "auto">
+                    <DropdownButton
+                        menuAlign="right"
+                        title="La Noria"
+                        id="dropdown-menu-align-right"
+                    >
+                        <Dropdown.Item eventKey="1">La Noria Pedregal</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item eventKey="2">Santo Chancho</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item eventKey="3">Santo Chancho Haye</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item eventKey="4">Irmaos Steaks and Burgers</Dropdown.Item>
+                    </DropdownButton>
+
+                </Col>
+                <Col xs lg="2">
+                <DropdownButton
+                    menuAlign="right"
+                    title="Restaurante"
+                    id="dropdown-menu-align-right"
+                >
+                    <Dropdown.Item eventKey="1">Restaurante</Dropdown.Item>
+
+                    <Dropdown.Divider />
+                    <Dropdown.Item eventKey="4">Irmaos Bar</Dropdown.Item>
+                </DropdownButton>
+                </Col>
+
+            </Row>
+
+
             <div className="row">
                 <div className="col-md-6 text-left contenido-mesas" >
-                    <Card className="mt-1" style={{ width: '18rem', height: "18rem", justifyContent: 'center' }}>
-                        <Card.Body>
-                            <Card.Title className="text-center font-weight-bold">
-                                Orden 1
-                            </Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">
-                                Terraza
-                            </Card.Subtitle>
-                            <Card.Subtitle className="mb-2 text-muted">
-                                12:30 23-06-2020
-                            </Card.Subtitle>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Picaña Haye" />
-                            </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Cerveza" />
-                            </Form.Group>
-                            <Card.Text>
-                                <br />
-                                Picania Haye
-                           </Card.Text>
-                            <Button variant="primary">Aceptar</Button>
-                            <Button variant="light">Rechazar</Button>
-                        </Card.Body>
-
-
-
-                    </Card>
+                    <OrderCard></OrderCard>
 
                 </div>
             </div>
