21611a2af2318fdffbc4adc6ad28f611d759479a
Esteban Cortés Lozano <corlo.esteban@gmail.com>
2021-03-08 09:17:40 -0600

    Update Tables.js

--- waitqr/src/components/Dashboard/Sidebar/Tables/Tables.js
+++ waitqr/src/components/Dashboard/Sidebar/Tables/Tables.js
@@ -45,7 +45,7 @@ function Tables() {
         guardarRestauranteEscogido(restaurante.nombre);
     }
     return (
-        <Container fluid>
+        <Container>
             <Row>
                 <Col sm={10}></Col>
                 <Col sm={2} className="dropdown-restaurante btn-group btn-block">
