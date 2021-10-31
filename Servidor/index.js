95a2b7a1a560458a196728ec41028fe6688c2fc1
javier-bonilla <javier.bonilla@glovoapp.com>
2021-10-31 20:37:01 +0100

    Initial production changes

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const conectarDB = require('./config/db');
-const cors = require('cors')
-
+const path = require("path");
+const express = require("express");
+const conectarDB = require("./config/db");
+const cors = require("cors");
 
 //crear el servidor
 const app = express();
-
 
 //Conectar a la base de datos
 conectarDB();
@@ -14,34 +13,29 @@ conectarDB();
 app.use(cors());
 
 //Habilitas express.json
-app.use(express.json({extend: true}));
+app.use(express.json({ extend: true }));
 
 //puerto de la app
 const PORT = process.env.PORT || 4000; //asigna el puerto o utiliza el 4000
 
 //rutas
-app.use('/api/usuario', require('./routes/usuario'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/restaurantes', require('./routes/restaurantes'));
-app.use('/api/menus', require('./routes/menus'));
-app.use('/api/secciones', require('./routes/secciones'));
-app.use('/api/platillos', require('./routes/platillos'));
-app.use('/api/mesas', require('./routes/mesas'));
-app.use('/api/sesionesgenerales', require('./routes/sesionesgenerales'));
-app.use('/api/sesionesindividuales', require('./routes/sesionesindividuales'));
-app.use('/api/ordenes', require('./routes/orden'));
-app.use('/api/platilloOrdenado', require('./routes/platilloOrdenado'));
-
-
-
-
-
-
-
-
-
+app.use("/api/usuario", require("./routes/usuario"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/restaurantes", require("./routes/restaurantes"));
+app.use("/api/menus", require("./routes/menus"));
+app.use("/api/secciones", require("./routes/secciones"));
+app.use("/api/platillos", require("./routes/platillos"));
+app.use("/api/mesas", require("./routes/mesas"));
+app.use("/api/sesionesgenerales", require("./routes/sesionesgenerales"));
+app.use("/api/sesionesindividuales", require("./routes/sesionesindividuales"));
+app.use("/api/ordenes", require("./routes/orden"));
+app.use("/api/platilloOrdenado", require("./routes/platilloOrdenado"));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 //arrancar la app
-app.listen(PORT, () =>{
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
