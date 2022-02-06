e04c0251b4033c376766f8c1bfb0b18d756fdeba
javier-bonilla <javier.bonilla@glovoapp.com>
2022-02-06 21:54:38 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/WaitqrApp/webapp""
    This reverts commit 815dac53150c78c75c1b2df6eb847d9f40ab9be4.

--- waitqr/src/components/Dashboard/Sidebar/Orders/OrderCard.js
+++ waitqr/src/components/Dashboard/Sidebar/Orders/OrderCard.js
@@ -16,31 +16,55 @@ import mesasContext from "../../../../context/mesas/mesasContext";
 function OrderCard(orden) {
   //obtener la funcion del context de platillo
   const platillosOrdenadosContext = useContext(platilloOrdenadoContext);
-  const { platilloOrdenadoOrden, obtenerPlatilloOrdenado } =
+  const { platilloOrdenadoOrden, obtenerPlatilloOrdenado, actualizarPlatilloOrdenado } =
     platillosOrdenadosContext;
+
+  const [botontomado, guardarBotonTomado] = useState(true)
 
   useEffect(() => {
     obtenerPlatilloOrdenado(orden.orden._id);
+
   }, [orden]);
 
   var pagar;
-  if(orden.orden.pagar == true){
+  if (orden.orden.pagar == true) {
     pagar = "pagar"
-    console.log("pagar")
   }
-  else{
+  else {
     pagar = ""
-    console.log(" no pagar")
+  }
+  console.log(botontomado)
+
+  
+
+  var platilloOrdenadoAux
+
+  const tomado = e => {
+    platilloOrdenadoOrden
+      .filter(({ orden: ordenOpt }) => ordenOpt === orden.orden._id)
+      .map((platilloOrdenado, index) => {
+
+        if (platilloOrdenado.tomado == false) {
+          platilloOrdenado.tomado = true
+          actualizarPlatilloOrdenado(platilloOrdenado)
+          //guardarBotonTomado(false)
+        }
+
+
+      }
+      )
   }
 
- // console.log({ platilloOrdenadoOrden, ordenId: orden.orden._id });
+
+
+  // console.log({ platilloOrdenadoOrden, ordenId: orden.orden._id });
   return (
-    <Card className={`tarjeta-orden ${pagar}` } style={{ justifyContent: "center" }}>
+    <Card className={`tarjeta-orden ${pagar}`} style={{ justifyContent: "center" }}>
       <Card.Body className="">
         <Card.Title className="text-center font-weight-bold">Orden</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{orden.orden.mesaNombre}</Card.Subtitle>
         <Card.Subtitle className="mb-2 text-muted font-weight-bold">
-        {orden.orden.registro.toString().substring(11, 16)} -{" "}
+          {orden.orden.registro.toString().substring(11, 16)} -{" "}
           {orden.orden.registro.toString().substring(0, 10)}
 
         </Card.Subtitle>
@@ -48,27 +72,41 @@ function OrderCard(orden) {
           {platilloOrdenadoOrden
             .filter(({ orden: ordenOpt }) => ordenOpt === orden.orden._id)
             .map((platilloOrdenado, index) =>
-            
+
               platilloOrdenado.ordenado == true ? (
-                index == platilloOrdenadoOrden.length?(
+                index == platilloOrdenadoOrden.length ? (
                   <ul id="nuevoPlatilloOrdenado">
-                  <h5>
-                    {platilloOrdenado.nombre} X {platilloOrdenado.cantidad}
-                  </h5>
-                  <p>{platilloOrdenado.comentario}</p>
-                </ul>
-                ):
-                <ul >
-                  <h5>
-                    {platilloOrdenado.nombre} X {platilloOrdenado.cantidad}
-                  </h5>
-                  <p>{platilloOrdenado.comentario}</p>
-                </ul>
+                    <h5>
+                      {platilloOrdenado.nombre} X {platilloOrdenado.cantidad}
+                    </h5>
+                    <p>{platilloOrdenado.comentario}</p>
+                  </ul>
+                ) :
+                  <ul >
+                    <h5>
+                      {platilloOrdenado.nombre} X {platilloOrdenado.cantidad}
+                    </h5>
+                    <p>{platilloOrdenado.comentario}</p>
+                  </ul>
               ) : (
                 <span></span>
               )
             )}
         </Card.Body>
+        {
+          botontomado ?
+            <Button
+              className="boton-orden-tomado"
+              variant="primary"
+              onClick={tomado}
+
+            >
+              Tomado
+            </Button>
+            :
+            <span></span>
+        }
+
         <Button
           className="boton-orden-aceptar"
           variant="primary"
