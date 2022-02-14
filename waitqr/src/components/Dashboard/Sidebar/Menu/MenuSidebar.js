24abbc23357b13a80546aba31f4fe419f2db4c6e
javier-bonilla <javier.bonilla@glovoapp.com>
2022-02-14 20:54:35 +0100

    Updates sidebar menu scroll

--- waitqr/src/components/Dashboard/Sidebar/Menu/MenuSidebar.js
+++ waitqr/src/components/Dashboard/Sidebar/Menu/MenuSidebar.js
@@ -1,71 +1,66 @@
-import React, {useState, useEffect, useContext} from "react";
-import { Nav, Button } from "react-bootstrap";
-import './menusidebar.css';
-import AddCategory from './AddCategory';
-import AddRestaurant from './AddRestaurant';
-import EditCategoryModal from './EditCategoryModal';
-import AddCategoryButton from './AddCategoryButton';
-import DeleteCategoryModal from './DeleteCategoryModal';
-import menusContext from '../../../../context/menus/menusContext';
-import seccionesContext from '../../../../context/secciones/seccionesContext';
-import AlertaContext from '../../../../context/alertas/alertaContext';
+import React, { useState, useEffect, useContext } from "react";
+import { Nav, Button, Row } from "react-bootstrap";
+import "./menusidebar.css";
+import AddCategory from "./AddCategory";
+import AddRestaurant from "./AddRestaurant";
+import EditCategoryModal from "./EditCategoryModal";
+import AddCategoryButton from "./AddCategoryButton";
+import DeleteCategoryModal from "./DeleteCategoryModal";
+import menusContext from "../../../../context/menus/menusContext";
+import seccionesContext from "../../../../context/secciones/seccionesContext";
+import AlertaContext from "../../../../context/alertas/alertaContext";
 
-function MenuSideBar(){
-    
+function MenuSideBar() {
+  //Extraer si un menu esta activo
+  const menussContext = useContext(menusContext);
+  const { menu } = menussContext;
 
-    //Extraer si un menu esta activo
-    const menussContext = useContext(menusContext); 
-    const {menu } = menussContext;
-    
+  //obtener la funcion del context de seccion
+  const seccionessContext = useContext(seccionesContext);
+  const { seccion, seccionesmenu, obtenerSecciones, guardarSeccionActual } =
+    seccionessContext;
 
+  const alertaContext = useContext(AlertaContext);
+  const { alerta, mostrarAlerta } = alertaContext;
 
-    //obtener la funcion del context de seccion
-    const seccionessContext = useContext(seccionesContext);
-    const {seccion,seccionesmenu, obtenerSecciones, guardarSeccionActual} = seccionessContext;
-
-    const alertaContext = useContext(AlertaContext);
-    const {alerta, mostrarAlerta} = alertaContext;
-
-
-    //obtener secciones cuando carga el componente
-    useEffect(() =>{
-
-        if(menu){
-            obtenerSecciones(menu[0]._id)
-        }
-        
-    }, [menu]); //para que corra solo una vez
-    
-    const seleccionarSeccion = seccion =>{
-        guardarSeccionActual(seccion._id)
-        obtenerSecciones(menu[0]._id)
+  //obtener secciones cuando carga el componente
+  useEffect(() => {
+    if (menu) {
+      obtenerSecciones(menu[0]._id);
     }
+  }, [menu]); //para que corra solo una vez
 
-    return (
-        <>
-            <Nav className="col-md-12 d-none d-md-block sidebar-menu"
+  const seleccionarSeccion = (seccion) => {
+    guardarSeccionActual(seccion._id);
+    obtenerSecciones(menu[0]._id);
+  };
+
+  return (
+    <>
+      <Nav className="col-md-12 d-none d-md-block sidebar-menu">
+        <Nav.Item className="mx-auto">
+          <Nav.Link className="sidebar-title" href="">
+            Secciones
+          </Nav.Link>
+        </Nav.Item>
+        {seccionesmenu.map((seccion) => (
+          <Nav.Item className="mx-auto">
+            <Nav.Link
+              className="sidebar-text-active"
+              onClick={() => seleccionarSeccion(seccion)}
             >
-                <div className="sidebar-sticky"></div>
-                <Nav.Item className="mx-auto">
-                    <Nav.Link className="sidebar-title" href="">Secciones</Nav.Link>
-                </Nav.Item>
-                {seccionesmenu.map(seccion=>(
-                <Nav.Item className="mx-auto">
-                    <Nav.Link className="sidebar-text-active" onClick={() => seleccionarSeccion(seccion)}>{seccion.nombre}
-                    <Button className="boton-editar-seccion" variant="light">
-                         <EditCategoryModal
-                            seccion = {seccion}
-                         ></EditCategoryModal>
-                         </Button>
-                    </Nav.Link>
-                </Nav.Item>
-                ))}
-                
-                <Nav.Item>
-                    <AddCategoryButton/>
-                </Nav.Item>
-            </Nav>
-        </>
-    );
+              {seccion.nombre}
+              <Button className="boton-editar-seccion" variant="light">
+                <EditCategoryModal seccion={seccion}></EditCategoryModal>
+              </Button>
+            </Nav.Link>
+          </Nav.Item>
+        ))}
+        <Nav.Item>
+          <AddCategoryButton />
+        </Nav.Item>
+      </Nav>
+    </>
+  );
 }
 export default MenuSideBar;
