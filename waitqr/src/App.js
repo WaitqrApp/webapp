9f95dc21710a313cf6d7495ab930324e424c58df
hyzmael88 <bartttt15@hotmail.com>
2022-02-20 00:05:10 -0600

    hashrouter

--- waitqr/src/App.js
+++ waitqr/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, useLocation} from "react-router-dom";
+import { HashRouter as Router, Switch, Route} from "react-router-dom";
 
 
 import Dashboard from './components/Dashboard/Dashboard';
