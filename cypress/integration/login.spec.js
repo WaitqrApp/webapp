e201a896cbfd0c44fcfda5886e97c5d32203418e
Esteban Cortés Lozano <26331769+esteban9706@users.noreply.github.com>
2022-02-19 20:53:53 -0600

    cypress tests added

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,17 @@
+// waitqrapp.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('waitqrapp login test', function () {
+    it('logs in from waitqrapp.com', function () {
+        cy.visit('https://waitqrapp.com')
+        cy.contains('Iniciar Sesión').click()
+        cy.get('#email').type('intento1@intento1.com')
+        cy.get('#password').type('intento123')
+        cy.get('#root > div > div > form > div:nth-child(3) > input').click()
+
+    })
+})
